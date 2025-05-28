07ecc0db0041e09be390b094f90ac72da184aeda
LucasAlbuquerquesousa <lucasalbuquerquessilva10@gamil.com>
2025-05-28 15:36:01 -0300

    Primeiro - commit Projeto Mario

---
+++ Mario/scripts.js
@@ -0,0 +1,24 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+const masc = document.querySelector(".mascara-formulario")
+
+const header = document.querySelector(".header")
+
+function cliqueiNoBotao() {
+    form.style.left = "39%"
+    form.style.transform = "translateX (-50%)"
+    mascara.style.visibility = "visible"
+}
+
+function mascaraClique() {
+    mascara.style.visibility = "hidden"
+    form.style.left = "-300px"
+    form.style.top = "30%"
+}
+
+function Header() {
+    form.style.left = "37%"
+    form.style.transform = "translateX (-50%)"
+    mascara.style.visibility = "visible"
+}
